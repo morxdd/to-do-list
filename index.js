9d0d625152319207df9c1be4c4e16232dfb0383e
morxdd <pomin1995qoot@gmail.com>
2024-10-19 05:51:10 +0800

    feat: 刪除項目

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-let inputValue =document.getElementById('todo-input');
+let inputValue = document.getElementById('todo-input');
 let addButton = document.getElementById('add-btn');
 
-addButton.addEventListener('click',function(){
+addButton.addEventListener('click', function () {
     console.log(inputValue.value);
     let newTodoListItem = document.createElement('li');
+    // 定義 X 按鈕 
+    let newCloseButton = document.createElement('button')
+    // 把button元素添加類名
+    newCloseButton.className = 'close-btn';
+
     // li 的內容寫入 inputValue 的 value
     newTodoListItem.innerText = inputValue.value;
 
-    // 抓取 ul 新增剛剛建立好的 li
+    // 在 li 中加入剛剛定義的X按鈕
+    newTodoListItem.appendChild(newCloseButton);
+
+    // 抓取 ul 
     let todoList = document.getElementById('todo-list');
+    // 新增剛剛建立好的 li
     todoList.appendChild(newTodoListItem);
+    // 清空輸入框
     inputValue.value = '';
+
+    newCloseButton.addEventListener('click', function () {
+        alert('刪除：' + newTodoListItem.innerText );
+        newTodoListItem.remove();
+
+    })
 })
+
