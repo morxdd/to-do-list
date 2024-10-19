902ddbc1fb26c0256143727c76a35d01fa4ee05b
morxdd <pomin1995qoot@gmail.com>
2024-10-19 21:30:19 +0800

    feat: 新增checkbox li文字用appendChild寫入 不為空才能add

--- index.js
+++ index.js
@@ -2,30 +2,45 @@ let inputValue = document.getElementById('todo-input');
 let addButton = document.getElementById('add-btn');
 
 addButton.addEventListener('click', function () {
-    console.log(inputValue.value);
-    let newTodoListItem = document.createElement('li');
-    // 定義 X 按鈕 
-    let newCloseButton = document.createElement('button')
-    // 把button元素添加類名
-    newCloseButton.className = 'close-btn';
+    if (inputValue.value !== '') {
 
-    // li 的內容寫入 inputValue 的 value
-    newTodoListItem.innerText = inputValue.value;
+        console.log(inputValue.value);
+        // 定義 li
+        let newTodoListItem = document.createElement('li');
 
-    // 在 li 中加入剛剛定義的X按鈕
-    newTodoListItem.appendChild(newCloseButton);
 
-    // 抓取 ul 
-    let todoList = document.getElementById('todo-list');
-    // 新增剛剛建立好的 li
-    todoList.appendChild(newTodoListItem);
-    // 清空輸入框
-    inputValue.value = '';
+        // 定義checkbox
+        let newCheckbox = document.createElement('input');
+        newCheckbox.type = 'checkbox';
 
-    newCloseButton.addEventListener('click', function () {
-        alert('刪除：' + newTodoListItem.innerText );
-        newTodoListItem.remove();
+        // 定義 X button按鈕 
+        let newCloseButton = document.createElement('button')
+        // 把button元素添加類名
+        newCloseButton.className = 'close-btn';
 
-    })
+
+
+        // 定義 text
+        let newItemText = document.createTextNode(inputValue.value);
+
+
+        // li的內容加入checkbox text button
+        newTodoListItem.appendChild(newCheckbox);
+        newTodoListItem.appendChild(newItemText);
+        newTodoListItem.appendChild(newCloseButton);
+
+        // 抓取 ul 
+        let todoList = document.getElementById('todo-list');
+        // 新增剛剛建立好的 li
+        todoList.appendChild(newTodoListItem);
+        // 清空輸入框
+        inputValue.value = '';
+
+        newCloseButton.addEventListener('click', function () {
+            // alert('刪除：' + newTodoListItem.innerText );
+            newTodoListItem.remove();
+
+        })
+    }
 })
 
