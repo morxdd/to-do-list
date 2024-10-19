8cd8fbb87ca539774c991706d509fcbf71043faf
morxdd <pomin1995qoot@gmail.com>
2024-10-20 00:02:02 +0800

    feat: checkbox增加刪除線 引入closeIcon

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import { closeIcon } from "./svg-icons.js";
+
 let inputValue = document.getElementById('todo-input');
 let addButton = document.getElementById('add-btn');
 
+// 監聽 inputValue 輸入事件
+inputValue.addEventListener('input', function () {
+    if (inputValue.value.trim() !== '') {
+        addButton.classList.add('active');
+    } else {
+        addButton.classList.remove('active');
+    }
+})
+
+
+// addButton 監聽點擊事件
 addButton.addEventListener('click', function () {
-    if (inputValue.value !== '') {
+    if (inputValue.value.trim() !== '') {
 
         console.log(inputValue.value);
         // 定義 li
@@ -17,11 +30,13 @@ addButton.addEventListener('click', function () {
         let newCloseButton = document.createElement('button')
         // 把button元素添加類名
         newCloseButton.className = 'close-btn';
+        // button 插入SVG
+        newCloseButton.innerHTML = closeIcon;
 
-
-
-        // 定義 text
-        let newItemText = document.createTextNode(inputValue.value);
+        // 去除輸入值的前後空白
+        let trimmedInputValue = inputValue.value.trim();
+        // 定義 text，把修剪過的輸入值拿來用
+        let newItemText = document.createTextNode(trimmedInputValue);
 
 
         // li的內容加入checkbox text button
@@ -39,8 +54,17 @@ addButton.addEventListener('click', function () {
         newCloseButton.addEventListener('click', function () {
             // alert('刪除：' + newTodoListItem.innerText );
             newTodoListItem.remove();
-
+        })
+        // 監聽 checkbox
+        newCheckbox.addEventListener('change', function () {
+            if (newCheckbox.checked) {
+                newTodoListItem.classList.add('completed')
+            } else {
+                newTodoListItem.classList.remove('completed')
+            }
         })
     }
 })
+
+
 
